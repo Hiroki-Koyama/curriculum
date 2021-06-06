f87a9610204aa5b07cb45bfe8b09e7dae705bb88
Hiroki-Koyama <65246035+Hiroki-Koyama@users.noreply.github.com>
2021-06-07 00:00:13 +0900

    変数・定数課題提出
    1-2の作成

---
+++ JavaScript/1-2/main.js
@@ -0,0 +1,11 @@
+let name = "hoge";
+//nameと書いたのに、hogeと出力されるはずです
+console.log(name);
+
+
+//問1：次のコードを実行すると、エラーになります。エラー箇所を特定して、200と1000が出力されるように修正してください。
+let price = 100;
+console.log(price * 2);
+
+price = 500;
+console.log(price * 2);
