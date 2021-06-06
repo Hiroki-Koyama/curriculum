04362a8146e1095aaff6baaf52af8cde4a52668a
Hiroki-Koyama <65246035+Hiroki-Koyama@users.noreply.github.com>
2021-06-06 22:46:35 +0900

    Hello World提出課題
    1-1の作成

---
+++ JavaScript/1-1/main.js
@@ -0,0 +1,26 @@
+console.log("Hello World!!");
+console.log(65536);
+console.log(10 + 5);
+console.log(10 * 5);
+console.log("Hello World!!");
+console.log("Hello " + "World!!");
+// 割り算の余りの値を求めたい場合は「%」を使用します。
+console.log(10 % 2);
+/*
+上記の場合、10割る2で余り0
+そのため出力は「0」
+下記の場合、出力は「1」になります
+*/
+console.log(10 % 3);
+
+
+
+//問1：コンソールに「10 + 8」と表示させてください。
+console.log("10 + 8");
+//問2：コンソールに10 + 8の結果を表示させてください。
+console.log(10 + 8);
+//問3：コンソールに「20割る3の余りは」と表示させ、続けてその計算結果を表示させてください。
+console.log("20割る3の余りは" + 20 % 3);
+
+
+window.alert("Hello World!!");
