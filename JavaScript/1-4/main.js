61460b71984076708b57f0cca96d94df8b475997
Hiroki-Koyama <65246035+Hiroki-Koyama@users.noreply.github.com>
2021-06-27 23:56:40 +0900

    繰り返し処理課題再提出
    1-4の作成

--- JavaScript/1-4/main.js
+++ JavaScript/1-4/main.js
@@ -5,14 +5,14 @@
 //それを1〜100まで実施してください。
 let number = 1;
 while (number <= 100) {
-    if (number % 3 === 0) {
+    if (number % 3 === 0 && number % 5 === 0) {
+      console.log("FizzBuzz!!");
+    }
+    else if (number % 3 === 0) {
       console.log("Fizz!");
     }
     else if (number % 5 === 0) {
       console.log("Buzz!");
-    }
-    else if (number % 3 === 0 && number % 5 === 0) {
-      console.log("FizzBuzz!!");
     }
     else console.log(number);
     number++;
